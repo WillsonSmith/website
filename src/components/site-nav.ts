7dab104c9189eb7f92d5ced8b81e85c8a1aba215
Willson Smith <me@willsonsmith.com>
2023-07-13 14:57:50 -0400

    Move logic into 'config' file

--- src/components/site-nav.ts
+++ src/components/site-nav.ts
@@ -4,7 +4,6 @@ import { customElement } from 'lit/decorators.js';
 @customElement('site-nav')
 export class MyComponent extends LitElement {
   firstUpdated() {
-    console.log(window.location.pathname);
     const active = this.shadowRoot!.querySelector(
       `a[href="${window.location.pathname}"]`
     );
--- ssg.config.js
+++ ssg.config.js
@@ -1,3 +1,5 @@
+const watchForChanges = argv.includes('--watch');
+
 import { ssg, render as renderStatic } from 'ssg';
 
 import { writeFile } from 'fs/promises';
@@ -8,50 +10,88 @@ import { argv } from 'node:process';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const watch = argv.includes('--watch');
-
+import { watch } from 'chokidar';
+import { glob } from 'glob';
 import { findDependencies } from '@custom-elements-manifest/find-dependencies';
 
-const config = (ssgConfig) => {
+if (watchForChanges) {
   const dependencyMap = new Map();
 
-  ssgConfig.addWatchTarget('src/pages/**/*.ts', async (target, changeType) => {
-    const dependencies = (
-      await findDependencies([target], {
-        basePath: 'src/pages',
-      })
-    ).filter((dep) => !dep.includes('node_modules'));
-    dependencyMap.set(target, dependencies);
-
-    const filePath = join(__dirname, target);
-
-    const result = await renderStatic(filePath);
-    const markup = result.markup;
-    const htmlFilePath = filePath.replace('.ts', '.html');
-    if (markup) {
-      await writeFile(htmlFilePath, markup);
-    }
+  watch('src/pages/**/*.ts').on('all', async (event, path) => {
+    const dependencies = await fetchLocalDependencies(path, dirname(path));
+    dependencyMap.set(path, dependencies);
+    buildPage(path);
   });
 
-  ssgConfig.addWatchTarget('src/**/*.ts', async (target, changeType) => {
-    const fullTargetPath = `${__dirname}/${target}`;
+  watch('src/**/*.ts').on('all', async (event, path) => {
+    const fullTargetPath = `${__dirname}/${path}`;
     for (const [page, dependencies] of dependencyMap.entries()) {
       if (dependencies.includes(fullTargetPath)) {
-        const filePath = join(__dirname, page);
-
-        const result = await renderStatic(filePath);
-        const markup = result.markup;
-        const htmlFilePath = filePath.replace('.ts', '.html');
-        if (markup) {
-          await writeFile(htmlFilePath, markup);
-        }
+        buildPage(page);
       }
     }
   });
+} else {
+  glob('src/pages/**/*.ts').then((files) => {
+    for (const file of files) buildPage(file);
+  });
+}
 
-  return {
-    watch,
-  };
-};
+async function buildPage(page) {
+  const filePath = join(__dirname, page);
+  const result = (await renderStatic(filePath)).markup;
+  if (result) {
+    await writeFile(filePath.replace('ts', 'html'), result);
+  }
+}
 
-ssg(config);
+async function fetchLocalDependencies(target, basePath) {
+  const dependencies = await findDependencies([target], { basePath });
+  return dependencies.filter(
+    (dependency) => !dependency.includes('node_modules')
+  );
+}
+
+// const config = (ssgConfig) => {
+//   const dependencyMap = new Map();
+
+//   ssgConfig.addWatchTarget('src/pages/**/*.ts', async (target, changeType) => {
+//     const dependencies = (
+//       await findDependencies([target], {
+//         basePath: 'src/pages',
+//       })
+//     ).filter((dep) => !dep.includes('node_modules'));
+//     dependencyMap.set(target, dependencies);
+
+//     const filePath = join(__dirname, target);
+
+//     const result = await renderStatic(filePath);
+//     const markup = result.markup;
+//     const htmlFilePath = filePath.replace('.ts', '.html');
+//     if (markup) {
+//       await writeFile(htmlFilePath, markup);
+//     }
+//   });
+
+//   ssgConfig.addWatchTarget('src/**/*.ts', async (target, changeType) => {
+//     const fullTargetPath = `${__dirname}/${target}`;
+//     for (const [page, dependencies] of dependencyMap.entries()) {
+//       if (dependencies.includes(fullTargetPath)) {
+//         const filePath = join(__dirname, page);
+
+//         const result = await renderStatic(filePath);
+//         const markup = result.markup;
+//         const htmlFilePath = filePath.replace('.ts', '.html');
+//         if (markup) {
+//           await writeFile(htmlFilePath, markup);
+//         }
+//       }
+//     }
+//   });
+
+//   return {
+//     watch: watchForChanges,
+//   };
+// };
+
+// ssg(config);
