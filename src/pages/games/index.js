c2d3cd09203624a9ebbfcaa1e9d816069f212a07
Willson Smith <me@willsonsmith.com>
2023-07-24 11:12:22 -0400

    Add site header to games page

--- src/pages/games/index.js
+++ src/pages/games/index.js
@@ -1,11 +1,23 @@
-import { html } from 'lit';
+import { html, css } from 'lit';
+
+import '../../components/site-header.js';
 
 export { layout } from '../../layouts/indexLayout.js';
-
 export { games } from '../../data/games.js';
+
+export const styles = css`
+  site-header {
+    position: sticky;
+    top: 0;
+
+    padding-inline: var(--size-3);
+    padding-block: var(--size-2);
+  }
+`;
 
 export default async (/** @type {any} */ { games }) => {
   return html`
+    <site-header title="Willson • Games"></site-header>
     <h1>Games</h1>
     <ul role="list">
       ${games.map(
