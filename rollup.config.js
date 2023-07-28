52fdc65484aea242bce730d89669667e9a02e685
Willson Smith <me@willsonsmith.com>
2023-07-28 16:03:33 -0400

    remove requirement for intermediate HTML build step

--- rollup.config.js
+++ rollup.config.js
@@ -19,18 +19,45 @@ import summary from 'rollup-plugin-summary';
 import path from 'path';
 
 import { glob } from 'glob';
+import { dirname, join } from 'path';
+import { fileURLToPath, pathToFileURL } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 import postcss from 'postcss';
 import autoprefixer from 'autoprefixer';
 import cssnanoPlugin from 'cssnano';
 import postcssImport from 'postcss-import';
 
-const htmlFiles = glob.sync('src/pages/**/*.html').map((file) => {
-  return {
-    name: file.replace('src/pages/', ''),
-    path: file,
-  };
-});
+let htmlInputs = [];
+
+const pageFiles = glob.sync('src/pages/**/*.js');
+for (const page of pageFiles) {
+  const { markup } = await render(join(__dirname, page));
+  if (markup) {
+    const markupWithAbsoluteURLs = markup
+      .split('\n')
+      .map((line) => {
+        if (line.includes('file://')) {
+          if (line.includes('.js')) {
+            return line.replace(pathToFileURL(__dirname).href, __dirname);
+          } else {
+            return line.replace(pathToFileURL(__dirname).href, '');
+          }
+        }
+        return line;
+      })
+      .join('\n');
+
+    const name = page.replace('src/pages/', '').replace('.js', '.html');
+    htmlInputs.push({
+      name: name,
+      html: markupWithAbsoluteURLs,
+    });
+  }
+}
+
+import { render } from 'ssg';
 
 export default {
   output: {
@@ -43,8 +70,9 @@ export default {
   plugins: [
     importMetaAssets(),
     html({
+      input: htmlInputs,
+      rootDir: __dirname,
       absoluteBaseUrl: 'https://willsonsmith.com',
-      input: htmlFiles,
       minify: false,
       injectServiceWorker: true,
       serviceWorkerPath: 'build/sw.js',
