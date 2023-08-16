87fa547de6792e80d14bd371dcae2fe3d6078590
Willson Smith <me@willsonsmith.com>
2023-08-16 19:38:14 -0400

    Don't minify at first

--- _build-utils/es-builder.js
+++ _build-utils/es-builder.js
@@ -13,44 +13,34 @@ import { minifyHTMLLiterals } from 'minify-html-literals';
 
 import { writeFile } from 'fs/promises';
 
-esbuild({
-  entryPoints: [...glob.sync('src/**/*.js'), ...glob.sync('src/**/*.ts')],
-  bundle: false,
-  outdir: 'dist',
-  format: 'esm',
-  allowOverwrite: true,
-  sourcemap: true,
-  plugins: [
-    {
-      name: 'minify-html-templates',
-      setup(build) {
-        build.onEnd(async () => {
-          const htmlFiles = glob.sync('dist/**/*.js');
+function buildJavascript(minify = false) {
+  esbuild({
+    entryPoints: [...glob.sync('src/**/*.js'), ...glob.sync('src/**/*.ts')],
+    bundle: false,
+    outdir: 'dist',
+    format: 'esm',
+    allowOverwrite: true,
+    sourcemap: true,
+    plugins: [
+      ...[
+        minify && {
+          name: 'minify-html-templates',
+          setup(build) {
+            build.onEnd(async () => {
+              const htmlFiles = glob.sync('dist/**/*.js');
 
-          for (const htmlFile of htmlFiles) {
-            const contents = await readFile(htmlFile, 'utf8');
+              for (const htmlFile of htmlFiles) {
+                const contents = await readFile(htmlFile, 'utf8');
 
-            const minified = minifyHTMLLiterals(contents);
-            if (minified?.code) await writeFile(htmlFile, minified.code);
-          }
-        });
-      },
-    },
-  ],
-  // plugins: [
-  //   {
-  //     name: 'minify-html-templates',
-  //     setup(build) {
-  //
-  //       // build.onLoad({ filter: /\.ts$/ }, async args => {
-  //       //   const contents = await readFile(args.path, 'utf8');
-  //       //
-  //       //   const minified = minifyHTMLLiterals(contents);
-  //       //   if (minified?.code) return { contents: minified.code };
-  //       //
-  //       //   return { contents };
-  //       // });
-  //     },
-  //   },
-  // ],
-});
+                const minified = minifyHTMLLiterals(contents);
+                if (minified?.code) await writeFile(htmlFile, minified.code);
+              }
+            });
+          },
+        },
+      ].filter(Boolean),
+    ],
+  });
+}
+
+buildJavascript();
