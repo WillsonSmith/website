43962275d307000e6fcb963a329e1ca8bbb95207
Willson Smith <me@willsonsmith.com>
2024-11-21 12:55:39 -0500

    rm nl

--- web/index-extracted.js
+++ web/index-extracted.js
@@ -1,4 +1,36 @@
 
+if (customElements.get('email-link') == undefined) {
+    customElements.define('email-link', class extends HTMLElement {
+      constructor() {
+          super();
+          const email = this.textContent?.replace('[at]', '@');
+          if (email != undefined) {
+              const link = `<a href="mailto:${email}">${email}</a>`;
+              this.innerHTML = link;
+          }
+      }
+    })
+}
+if (customElements.get('raising-block') == undefined) {
+    customElements.define('raising-block', class extends HTMLElement {
+        constructor() {
+            super();
+            this.classList.add('ready');
+            this.intersectionObserver = new IntersectionObserver(
+                this.handleIntersection,
+                { threshold: [0.5] },
+            );
+
+            this.intersectionObserver.observe(this);
+        }
+
+        handleIntersection = (entries) => {
+            for (const entry of entries) {
+                this.classList.toggle('visible', entry.isIntersecting);
+            }
+        } 
+    })
+}
     if (customElements.get('h-image-swap') == undefined) {
         customElements.define('h-image-swap', class extends HTMLElement {
             images = [];
@@ -26,35 +58,3 @@
             }
         })
     }
-if (customElements.get('raising-block') == undefined) {
-    customElements.define('raising-block', class extends HTMLElement {
-        constructor() {
-            super();
-            this.classList.add('ready');
-            this.intersectionObserver = new IntersectionObserver(
-                this.handleIntersection,
-                { threshold: [0.5] },
-            );
-
-            this.intersectionObserver.observe(this);
-        }
-
-        handleIntersection = (entries) => {
-            for (const entry of entries) {
-                this.classList.toggle('visible', entry.isIntersecting);
-            }
-        } 
-    })
-}
-if (customElements.get('email-link') == undefined) {
-    customElements.define('email-link', class extends HTMLElement {
-      constructor() {
-          super();
-          const email = this.textContent?.replace('[at]', '@');
-          if (email != undefined) {
-              const link = `<a href="mailto:${email}">${email}</a>`;
-              this.innerHTML = link;
-          }
-      }
-    })
-}
