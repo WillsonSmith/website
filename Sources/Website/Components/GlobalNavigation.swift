d19f9ff80d5be776f332d3c32821a1150c09dfe0
Willson Smith <me@willsonsmith.com>
2024-11-21 16:00:59 -0500

    indentation

--- Sources/Website/Components/GlobalNavigation.swift
+++ Sources/Website/Components/GlobalNavigation.swift
@@ -6,47 +6,47 @@ struct GlobalNavigation: HTMLFragment {
 
   var css: String {
     """
-        .global-navigation {
-            color: var(--gray-1);
-            --font-size-title: var(--font-size-5);
-            --font-weight-title: var(--font-weight-6);
+    .global-navigation {
+        color: var(--gray-1);
+        --font-size-title: var(--font-size-5);
+        --font-weight-title: var(--font-weight-6);
 
-            position: sticky;
-            top: 0;
-            z-index: 10;
+        position: sticky;
+        top: 0;
+        z-index: 10;
 
+        display: flex;
+        align-items: center;
+
+
+        padding-block: var(--size-1);
+        padding-inline: var(--size-fluid-2);
+
+        .links {
             display: flex;
-            align-items: center;
+            gap: var(--size-3);
+        }
 
+        a {
+            text-decoration: none;
+            scale: 1;
+            transform-origin: 50% 100%;
+            transition: scale 100ms var(--ease-out-1);
 
-            padding-block: var(--size-1);
-            padding-inline: var(--size-fluid-2);
-
-            .links {
-                display: flex;
-                gap: var(--size-3);
-            }
-
-            a {
-                text-decoration: none;
-                scale: 1;
-                transform-origin: 50% 100%;
-                transition: scale 100ms var(--ease-out-1);
-
-                &:hover {
-                    scale: 1.1;
-                }
-            }
-
-            h1 {
-                flex: 1;
-                font-family: var(--font-rounded-sans);
-                font-size: var(--font-size-title);
-                font-weight: var(--font-weight-title);
-
-                /* line-height: var(--font-lineheight-1); */
+            &:hover {
+                scale: 1.1;
             }
         }
+
+        h1 {
+            flex: 1;
+            font-family: var(--font-rounded-sans);
+            font-size: var(--font-size-title);
+            font-weight: var(--font-weight-title);
+
+            /* line-height: var(--font-lineheight-1); */
+        }
+    }
     """
   }
 
