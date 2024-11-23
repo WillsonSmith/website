4952d66051433ff7ce1b81c8fbf6cb9540ded5f1
Willson Smith <me@willsonsmith.com>
2024-11-22 23:26:49 -0500

    fix words

--- web/index-extracted.js
+++ web/index-extracted.js
@@ -1,4 +1,3 @@
-
 if (customElements.get('h-image-swap') == undefined) {
     customElements.define('h-image-swap', class extends HTMLElement {
         images = [];
@@ -26,18 +25,6 @@ if (customElements.get('h-image-swap') == undefined) {
         }
     })
 }
-if (customElements.get('email-link') == undefined) {
-    customElements.define('email-link', class extends HTMLElement {
-      constructor() {
-          super();
-          const email = this.textContent?.replace('[at]', '@');
-          if (email != undefined) {
-              const link = `<a href='mailto:${email}'>${email}</a>`;
-              this.innerHTML = link;
-          }
-      }
-    })
-}
 if (customElements.get('raising-block') == undefined) {
     customElements.define('raising-block', class extends HTMLElement {
         constructor() {
@@ -56,5 +43,18 @@ if (customElements.get('raising-block') == undefined) {
                 this.classList.toggle('visible', entry.isIntersecting);
             }
         } 
+    })
+}
+
+if (customElements.get('email-link') == undefined) {
+    customElements.define('email-link', class extends HTMLElement {
+      constructor() {
+          super();
+          const email = this.textContent?.replace('[at]', '@');
+          if (email != undefined) {
+              const link = `<a href='mailto:${email}'>${email}</a>`;
+              this.innerHTML = link;
+          }
+      }
     })
 }
